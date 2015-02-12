a0ca50fc31bc74a71f5672c0663b11b495639fe6
protenchen <protenchen@hotmail.com>
2015-02-11 22:25:41 -0800

    Delete, save and update
    Implement delete, save and update functions

--- app/js/customer.js
+++ app/js/customer.js
@@ -1,17 +1,4 @@
 $(document).ready(function() {
-  $("#addCustomer").click(function() {
-    $("#customerInformation").hide();
-    $("#editForm").hide();
-    $("#informationForm").show();
-  });
-  $("#cancel_Add").click(function() {
-    $("#informationForm").hide();
-  });
-
-  $('#editCustomer').click(function() {
-    $("#customerInformation").hide();
-    $("#editForm").show();
-  });
 
   $("#cancel_Edit").click(function() {
     $("#editForm").hide();
